0d51c136e83feb605438837694c3b6d09cc02120
shoppiboi <ahasan.shoisob@gmail.com>
2021-06-30 20:38:45 +0100

    feat: add React 'Hello World!'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+
+const testContacts = [
+  {
+    id: 1,
+    name: 'John Smith',
+    phone_number: '1234567890',
+    email: 'john.smith@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'John Johnson',
+    phone_number: '1234567890',
+    email: 'john.johnson@gmail.com'
+  }
+]
+
+class App extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: testContacts,
+    };
+  }
+  
+  render() {  
+      return <h1>Hello world!</h1>;  
+  }  
 }
 
 export default App;
