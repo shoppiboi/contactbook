35f71d3ef6d1589207d25b6ba32ab80fcefe3e97
shoppiboi <ahasan.shoisob@gmail.com>
2021-06-30 19:06:47 +0100

    feat: add and register Contact model

--- backend/contactbook/admin.py
+++ backend/contactbook/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email')
 
 # Register your models here.
+
+admin.site.register(Contact, ContactAdmin)
