7655dd93f4db5ca03f574be6aabd49a36cf76a59
shoppiboi <ahasan.shoisob@gmail.com>
2021-07-01 22:05:58 +0100

    fix: solve import error for views.py in backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from backend.contactbook import views
+from contactbook import views
 
 router = routers.DefaultRouter()
 router.register(r'contactbook', views.ContactBookView, 'contact')
